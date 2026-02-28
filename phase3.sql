d327162387be7e28b1395089c87082ea6dfdc772
Zach Wu <zachary.h.wu@gmail.com>
2026-02-28 14:42:58 -0800

    Imported tabels on my localhost database

---
+++ phase3.sql
@@ -0,0 +1,9 @@
+USE INFO_330_ZJA
+
+-- Import all the csv files as staging files. 
+-- Coaches is called coaches on my screen
+-- Events is called events
+-- Student Athletes is called student_athletes
+SELECT * FROM teams
+
+
